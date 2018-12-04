73aeb0da2423777af207b08b56cb94becfa42eb4
So <quddbs30@naver.com>
2018-12-04 23:16:10 +0900

    add comments

--- ut/utDollar.cpp
+++ ut/utDollar.cpp
@@ -7,7 +7,10 @@ TEST(utDollar, testMultiplcation) {
     ASSERT_TRUE(*(new Dollar(10)) == *five->times(2));
     ASSERT_TRUE(*(new Dollar(15)) == *five->times(3));
 }
-TEST(utDollar, testEquality_p) {
+
+// 아래 두 test 에서 객체 생성 방법이 다르지만, 
+// == 연산자 사용 시 같은 결과를 얻는다. pointer 에 대한 이해가 필요함..
+TEST(utDollar, testEqualiy_p) {
     ASSERT_TRUE(*(new Dollar(5)) == *(new Dollar(5)));
 }
 TEST(utDollar, testEquality_n) {
